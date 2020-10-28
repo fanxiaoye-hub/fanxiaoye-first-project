16dcc3e37dc3339a91bf8453e6b74911e9a2e329
fanxiaoye-hub <zyf739977055>
2020-10-28 20:22:08 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atgui/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.atgui.git;
+
+/**
+ * @author 凡小爷
+ * @date 2020/10/28 - 20:07
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+                System.out.println("Hello Git !");
+    }
+}
